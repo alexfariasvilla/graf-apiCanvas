3373ce0697c58b492166e23cbeda848a87ee0fd6
Alexander Jahaziel Farias Villa <alexanderjahazielfariasvilla@MacBook-Air-de-Alexander.local>
2026-03-08 22:37:41 -0600

    fix canvas layout

--- main.js
+++ main.js
@@ -1,81 +1,69 @@
 function draw() {
 
 var canvas = document.getElementById("canvas");
-
 var ctx = canvas.getContext("2d");
 
+// RECTANGULO VERDE
 ctx.fillStyle = "green";
-
-ctx.fillRect(10, 10, 100, 100);
-
-function draw() {
-
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
-
-// CUADRADO VERDE
-ctx.fillStyle = "green";
-ctx.fillRect(10, 10, 100, 100);
+ctx.fillRect(20,20,100,100);
 
 // TRIANGULO
 ctx.beginPath();
-ctx.moveTo(75, 150);
-ctx.lineTo(150, 250);
-ctx.lineTo(0, 250);
+ctx.moveTo(200,20);
+ctx.lineTo(250,120);
+ctx.lineTo(150,120);
 ctx.closePath();
 ctx.fillStyle = "red";
 ctx.fill();
 
 // HAPPY FACE
 ctx.beginPath();
-ctx.arc(300, 150, 50, 0, Math.PI * 2, true);
-ctx.moveTo(335, 150);
-ctx.arc(300, 150, 35, 0, Math.PI, false);
-ctx.moveTo(290, 135);
-ctx.arc(285, 135, 5, 0, Math.PI * 2, true);
-ctx.moveTo(320, 135);
-ctx.arc(315, 135, 5, 0, Math.PI * 2, true);
+ctx.arc(380,70,40,0,Math.PI*2,true);
+ctx.moveTo(405,70);
+ctx.arc(380,70,25,0,Math.PI,false);
+ctx.moveTo(370,60);
+ctx.arc(365,60,5,0,Math.PI*2,true);
+ctx.moveTo(395,60);
+ctx.arc(390,60,5,0,Math.PI*2,true);
 ctx.stroke();
 
 // LINEAS
 ctx.beginPath();
-ctx.moveTo(50, 300);
-ctx.lineTo(200, 300);
-ctx.lineTo(200, 350);
-ctx.lineTo(50, 350);
+ctx.moveTo(20,180);
+ctx.lineTo(120,180);
+ctx.lineTo(120,230);
+ctx.lineTo(20,230);
 ctx.closePath();
 ctx.stroke();
 
 // ARCOS
 ctx.beginPath();
-ctx.arc(300, 300, 40, 0, Math.PI);
+ctx.arc(220,205,25,0,Math.PI);
 ctx.stroke();
 
 // CURVA CUADRATICA
 ctx.beginPath();
-ctx.moveTo(350, 300);
-ctx.quadraticCurveTo(400, 250, 450, 300);
+ctx.moveTo(300,180);
+ctx.quadraticCurveTo(350,140,400,180);
 ctx.stroke();
 
 // CURVA BEZIER
 ctx.beginPath();
-ctx.moveTo(350, 350);
-ctx.bezierCurveTo(380, 320, 420, 380, 450, 350);
+ctx.moveTo(300,240);
+ctx.bezierCurveTo(320,200,380,280,420,240);
 ctx.stroke();
 
-// COMBINACION DE FIGURAS
+// COMBINACION
 ctx.fillStyle = "purple";
-ctx.fillRect(50, 400, 80, 50);
+ctx.fillRect(20,300,80,50);
 
 ctx.beginPath();
-ctx.moveTo(50, 400);
-ctx.lineTo(130, 450);
+ctx.moveTo(20,300);
+ctx.lineTo(100,350);
 ctx.stroke();
 
 ctx.beginPath();
-ctx.arc(200, 425, 25, 0, Math.PI * 2);
+ctx.arc(160,325,25,0,Math.PI*2);
 ctx.stroke();
-
-}
 
 }
